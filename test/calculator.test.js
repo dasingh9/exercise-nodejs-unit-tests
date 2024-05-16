2cc93648c45a08a4d53772f4384af301663d91ac
Davinder Singh <dasingh9@gmail.com>
2024-05-16 16:21:33 +1200

    Add subtract, multiply, and divide functionality

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,13 +1,23 @@
 import { expect } from "chai";
-import { add, multiply } from "../calculator.js"
+import { add, subtract, multiply, divide } from "../calculator.js"
 
-it("should add two valid numbers", ()=> {
+it("should add two valid integer numbers", ()=> {
     const result = add(10, 20);
     expect(result).to.be.equal(30);
 });
 
-it("should multiply two valid numbers", ()=> {
-    const result = multiply(10, 20);
-    expect(result).to.be.equal(200);
+it("should subtract two valid integer numbers", ()=> {
+    const result = subtract(100, 20);
+    expect(result).to.be.equal(80);
+});
+
+it("should multiply two valid integer numbers", ()=> {
+    const result = multiply(1111, 1111);
+    expect(result).to.be.equal(1234321);
+});
+
+it("should divide two valid integer numbers", ()=> {
+    const result = divide(100, 25);
+    expect(result).to.be.equal(4);
 });
 
